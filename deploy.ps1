dd4ee7f5c94d1a06b1be62192f817d3da918fc75
Neo <zorthiha.neo@gmail.com>
2026-08-14 00:36:38 +0700

    fix: tag docker image with git commit SHA in deploy.ps1 for ECR tag immutability

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,7 @@
-docker build --provenance=false -t direhire/runtime -f Dockerfile .
+$TAG = (git rev-parse --short HEAD).Trim()
+docker build --provenance=false -t direhire/runtime:$TAG -f Dockerfile .
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 685134815483.dkr.ecr.ap-southeast-1.amazonaws.com
-docker tag direhire/runtime 685134815483.dkr.ecr.ap-southeast-1.amazonaws.com/direhire/runtime:latest
-docker push 685134815483.dkr.ecr.ap-southeast-1.amazonaws.com/direhire/runtime:latest
-aws lambda update-function-code --function-name direhire-prod-api --image-uri 685134815483.dkr.ecr.ap-southeast-1.amazonaws.com/direhire/runtime:latest --region ap-southeast-1
+docker tag direhire/runtime:$TAG 685134815483.dkr.ecr.ap-southeast-1.amazonaws.com/direhire/runtime:$TAG
+docker push 685134815483.dkr.ecr.ap-southeast-1.amazonaws.com/direhire/runtime:$TAG
+aws lambda update-function-code --function-name direhire-prod-api --image-uri 685134815483.dkr.ecr.ap-southeast-1.amazonaws.com/direhire/runtime:$TAG --region ap-southeast-1
+
